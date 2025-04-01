0aca5832df843ff8a8ca48cec9ba3ba37a306733
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-04-01 10:46:29 +0100

    Initial empty functions for the list added.

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# To Do List
+def add_task():
+    """
+    Allow users to add new task in the list.
+    """
+
+
+def show_task():
+    """
+    Allow users to see the list of tasks.
+    """
+
+
+def remove_task():
+    """
+    Allow the users to remove a particular task from the list
+    """
+
+
+def main():
+    """
+    The menu/user choices are written here.
+    """
